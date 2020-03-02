352af442229623d257853ec22cc0ff3599c036aa
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-03-02 10:46:59 +0100

    ajout début fonction alphabet laurine

---
+++ alphabet/alphabet_Laurine.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+print ("abcdefghijklmnopqrstuvxyz")
+
+def alphabet():
+	letter = 'a'
+	while letter <= 'z'
+		print(letter);
+		letter++;
+
+
+# s="cFf:^ggioipze........."
+# a=ord(s[0])
+# print a

eff7f38bd85aba040ef0b3d21af393565b7f4ac3
lydiasan <lyd.sanaa@gmail.com>
2020-03-03 09:21:26 +0100

    ajout du dossier Lydia

---
+++ alphabet/Lydia/alphabet_lydia.py
@@ -0,0 +1,9 @@
+# Ecrire une fonction qui affiche l'alphabet en minuscule sur une seule ligne et dans l'ordre.
+# Avec test unitaire.
+
+
+def affiche_alphabet():
+	alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	
+
+	# doc python : https://www.fil.univ-lille1.fr/~wegrzyno/portail/Info/Doc/HTML/seq4_chaines_caracteres.html

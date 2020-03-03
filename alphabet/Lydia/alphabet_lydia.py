d62ee5aaa6e76f18ebf5dd43d5448a01d12e1a6a
lydiasan <lyd.sanaa@gmail.com>
2020-03-03 09:54:31 +0100

    ajout fichier test

--- alphabet/Lydia/alphabet_lydia.py
+++ alphabet/Lydia/alphabet_lydia.py
@@ -6,4 +6,4 @@ def affiche_alphabet():
 	alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	
 
-	# doc python : https://www.fil.univ-lille1.fr/~wegrzyno/portail/Info/Doc/HTML/seq4_chaines_caracteres.html
+# doc python : https://www.fil.univ-lille1.fr/~wegrzyno/portail/Info/Doc/HTML/seq4_chaines_caracteres.html
---
+++ alphabet/Lydia/test_alphabet_lydia.py
@@ -0,0 +1,5 @@
+from alphabet_lydia import *
+import unittest
+
+class afficher_alphabet(unittest.TestCase):
+    

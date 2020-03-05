005e570eb2477751c650e288a9ce4abb1069303e
Wara-nyu <w.bounkeua@gmail.com>
2020-03-05 12:17:36 +0100

    renome et rectifie le test

---
+++ alphabet/Del/alphabet.py
@@ -0,0 +1,12 @@
+import string
+
+# -*- coding: utf-8 -*-
+
+def minus(a) :
+    return string.ascii_lowercase
+
+def maj(b) :
+    return string.ascii_uppercase
+
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
---
+++ alphabet/Del/testAlphabet.py
@@ -0,0 +1,10 @@
+import unittest
+from fonctionD import *
+
+class alphabet(unittest.TestCase):
+    def test_revoie_minus(self):
+        self.assertEqual("abcdefghijklmnopqrstuvwxyz", minus(''))
+    def test_revoie_maj(self):
+        self.assertEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ", maj(''))
+
+unittest.main()

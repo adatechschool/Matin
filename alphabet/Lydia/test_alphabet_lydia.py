e55c971665df9418d285944170a526ab79e3162e
lydiasan <lyd.sanaa@gmail.com>
2020-03-09 09:41:08 +0100

    la fonction renvoie l'alphabet.

--- alphabet/Lydia/test_alphabet_lydia.py
+++ alphabet/Lydia/test_alphabet_lydia.py
@@ -1,5 +1,8 @@
-from alphabet_lydia import *
 import unittest
+from alphabet_lydia import *
 
 class afficher_alphabet(unittest.TestCase):
-    
+    def test_alphabet(self):
+        self.assertEqual("abcdefghijklmnopqrstuvwxyz", affiche_alphabet())
+
+unittest.main()

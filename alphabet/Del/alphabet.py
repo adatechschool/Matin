c53ecbf819d22bce603e84ffe5dc64ac30e8586b
Leirda01 <adrieldj@orange.fr>
2020-03-09 08:53:06 +0000

    correction

--- alphabet/Del/alphabet.py
+++ alphabet/Del/alphabet.py
@@ -2,10 +2,10 @@ import string
 
 # -*- coding: utf-8 -*-
 
-def minus(a) :
+def minus(a="") :
     return string.ascii_lowercase
 
-def maj(b) :
+def maj(b="") :
     return string.ascii_uppercase
 
 print(string.ascii_lowercase)
--- alphabet/Del/testAlphabet.py
+++ alphabet/Del/testAlphabet.py
@@ -1,10 +1,10 @@
 import unittest
-from fonctionD import *
+from alphabet import *
 
 class alphabet(unittest.TestCase):
     def test_revoie_minus(self):
-        self.assertEqual("abcdefghijklmnopqrstuvwxyz", minus(''))
+        self.assertEqual("abcdefghijklmnopqrstuvwxyz", minus())
     def test_revoie_maj(self):
-        self.assertEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ", maj(''))
+        self.assertEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ", maj())
 
 unittest.main()

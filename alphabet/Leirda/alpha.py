5cc5c5ad42413fed536ee4125a79fa7407c2620c
Leirda01 <adrieldj@orange.fr>
2020-03-11 08:42:43 +0000

    Termine l’exercice avec liste[x:y] en python.

--- alphabet/Leirda/alpha.py
+++ alphabet/Leirda/alpha.py
@@ -1,2 +1,4 @@
-def alpha_low(optional_range = "a-z"):
-    return "abcdefghijklmnopqrstuvwxyz"
+αβ = "abcdefghijklmnopqrstuvwxyz"
+
+def alpha_low(opt_range = "a-z"):
+    return αβ[αβ.index(opt_range[0]):αβ.index(opt_range[2]) + 1]

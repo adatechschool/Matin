4a21f6a1eebfacd56ee4ab0d449de314be79834b
Leirda01 <adrieldj@orange.fr>
2020-05-15 10:44:20 +0200

    Fais passer tous les tests de l’exemple

--- mastermind/Leirda/mastermind.py
+++ mastermind/Leirda/mastermind.py
@@ -7,4 +7,6 @@ Color = enum.Enum('Color', 'red blue green pink yellow purple')
 def hint(secret, guess):
     if secret[0] == guess[0]:
         return [1, 0]
+    if secret[0] in guess:
+        return [0, 1]
     return [0, 0]
--- mastermind/Leirda/test.py
+++ mastermind/Leirda/test.py
@@ -6,5 +6,7 @@ class give_hint(unittest.TestCase):
         self.assertEqual(hint([Color.blue], [Color.red]), [0, 0])
     def test_1_well_placed(self):
         self.assertEqual(hint([Color.blue], [Color.blue]), [1, 0])
+    def test_1_misplaced(self):
+        self.assertEqual(hint([Color.red, Color.yellow], [Color.blue, Color.red]), [0, 1])
 
 unittest.main()

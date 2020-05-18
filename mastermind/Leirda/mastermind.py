eb017ef591ee41ee54c2dbf8ac02f6b79dd59ce9
Leirda01 <adrieldj@orange.fr>
2020-05-18 09:53:21 +0200

    Ignore les guess et secret de tailles différentes

--- mastermind/Leirda/mastermind.py
+++ mastermind/Leirda/mastermind.py
@@ -7,14 +7,9 @@ Color = enum.Enum('Color', 'red blue green pink yellow purple')
 def hint(secret, guess):
     good = 0
     miss = 0
-    for color in zip(guess, list_pad(secret, len(guess) - len(secret))):
+    for color in zip(guess, secret):
         if color[0] == color[1]:
             good += 1
         elif color[0] in secret:
             miss += 1
     return [good, miss]
-
-def list_pad(list, size):
-    for elem in range(size):
-        list += [None]
-    return list
--- mastermind/Leirda/test.py
+++ mastermind/Leirda/test.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import unittest
 from mastermind import *
+
 
 class give_mastermind_code_hint(unittest.TestCase):
     def test_no_clue(self):
@@ -10,12 +12,6 @@ class give_mastermind_code_hint(unittest.TestCase):
         self.assertEqual(hint([Color.red, Color.yellow], [Color.blue, Color.red]), [0, 1])
 
     def test_1_well_placed_1_misplaced(self):
-        self.assertEqual(hint([Color.red, Color.yellow], [Color.red, Color.pink, Color.yellow]), [1, 1])
-
-class pad_none_to_list(unittest.TestCase):
-    def test_list_pad(self):
-        self.assertEqual(list_pad([0, 1, 1], 3), [0, 1, 1, None, None, None])
-    def test_list_pad_negative(self):
-        self.assertEqual(list_pad(["hello", ", world!"], -3), ["hello", ", world!"])
+        self.assertEqual(hint([Color.red, Color.yellow, Color.green], [Color.red, Color.pink, Color.yellow]), [1, 1])
 
 unittest.main()
